5908e075e147a678d315b47a3518ecbe66a34e3d
Alexander Shen <70786647+shenalexw@users.noreply.github.com>
2022-05-24 13:39:51 -0700

    fix cors

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use cors
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*"
+  }))
+
 app.use('/', indexRouter);
 app.use('/image', imageRouter);
 
